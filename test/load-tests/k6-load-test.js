16df31889600e23faef50f067dafd56502b5044d
Thiago Thalison Firmino de Lima <thiago.firmino@intelbras.com.br>
2024-07-30 19:59:35 -0300

    infra: update load testes

--- test/load-tests/k6-load-test.js
+++ test/load-tests/k6-load-test.js
@@ -5,8 +5,8 @@ import { check, sleep } from 'k6';
 // Define the options for the test
 export let options = {
   stages: [
-    { duration: '1m', target: 1000 }, 
-    { duration: '5m', target: 5000 }, 
+    { duration: '1m', target: 3000 }, 
+    { duration: '1m', target: 5000 }, 
     { duration: '2m', target: 0 }, 
   ],
 };
